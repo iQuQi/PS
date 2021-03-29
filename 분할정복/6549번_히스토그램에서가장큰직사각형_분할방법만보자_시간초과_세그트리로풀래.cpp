a15664e18935632428fb48fab2981277b26a3f82
iQuQi <69742775+iQuQi@users.noreply.github.com>
2021-03-29 18:09:38 +0900

    0329_1

--- 분할정복/6549번_히스토그램에서가장큰직사각형_분할방법만보자_시간초과_세그트리로풀래.cpp
+++ 분할정복/6549번_히스토그램에서가장큰직사각형_분할방법만보자_시간초과_세그트리로풀래.cpp
@@ -6,74 +6,14 @@
 #include <string>
 #include <algorithm>
 #include <climits>
-//히스토그램은 직사각형 여러 개가 아래쪽으로 정렬되어 있는 도형이다.
-//각 직사각형은 같은 너비를 가지고 있지만, 높이는 서로 다를 수도 있다.
-//히스토그램에서 가장 넓이가 큰 직사각형을 구하는 프로그램을 작성하시오.
 
-//모든 직사각형의 너비는 1
-//테스트 케이스는 여러개 - 한줄씩 한 테스트
-//입력의 마지막 줄 에는 0이 하나 주어짐
-
-//<접근법>
-//1.start~cut-1까지
-//2.cut 포함 구간
-//cut= 가장 낮은 애를 기준으로 반을 가름
-//그럼 그 mid를 포함하는 넓이는 당연히 그높이*n
-//3.mid+1~end 까지 최대값을 각각 구하고
-//이중에서 최댓값을 찾는다
-
+//
 using namespace std;
-int n;
-int index = 0;
-int input;
-vector<pair<int,int> >hei;
-
-
-
-
-long long histo(int start, int end) {
-
-	if(start<end){
-		//벡터 범위 내에서 최소값 구하기
-		//O(N) -> 시간 초과ㅠㅠ
-		int cut = (*min_element(hei.begin() + start, hei.begin() + end)).second;
-		
-		//왼쪽 구간에서의 최댓값
-		long long left_max=histo(start,cut-1);
-		//오른쪽 구간에서의 최댓값
-		long long right_max = histo(cut+1, end);
-		//자른지점을 포함하는 최댓값
-		long long mid_max = hei[cut].first * (end - start + 1);
-
-		//오른쪽 왼쪽 중에 최대
-		long long tmp = left_max < right_max ? right_max : left_max;
-		//최종 최댓값
-		return mid_max < tmp ? tmp : mid_max;
-		
-	}
-	return hei[start].first;
-}
 
 
 
 int main() {
 	
-	while (scanf("%d", &n) && n != 0) {
-	
-		//각 직사각형의높이 입력받기
-		for (int i = 0; i < n; i++)
-		{
-			cin >> input;
-			hei.push_back({ input,i });
 
-		}
-		printf("%lld\n",histo(0, n - 1));
-		hei.clear();
-		
-	}
-
-
-	//cf)일차원 배열 초기화=memset(arr, 0, sizeof(arr));
-		//이차원 배열 초기화=memset(array, 0, sizeof(array[0][0] * ROW_MAX * COL_MAX)
 }
 
---
+++ 파라미터 탐색/2792번_보석상자.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#define CON 1000000000 
+using namespace std;
+int n, m, need;
+vector<int > v;
+//질투심이 최소가 되게 보석 나누기 -> 질투심 최솟값 출력
+//모두가 받지는 않아도 되지만 학생은 항상 같은 색상의 보석만 가짐
+//질투심은 가장 많은 보석을 가져간 학생의 보석 개수
+
+//모든 보석을 나누어 줄 수 있는가
+int too(int per) {
+	int ans = 0;
+	for (int i = 0; i < m; i++)
+	{
+		ans += v[i] / per;//보석의 종류별로 per 만큼씩 줬을때 몇명에게 줄수있는지 합계
+		if (v[i] % per != 0) ans++;//나누어 떨어지지 않는 경우 나머지를 한명에 줄 수 있음
+	}
+
+	//모든 보석 사용 가능
+	if (ans <= n)return 1;
+	//불가능
+	return 0;
+}
+
+
+//이분탐색 -> parametric 
+//모든 보석을 나눠 줄 수 있는 경우 중 per이 최소로 되는 값 구하기
+int bin(int low, int high) {
+	int mid, ret = high;
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		//모든 보석을 나눠줄 수 있는 경우
+		if (too(mid)) {
+			//ret값(min 값) 갱신
+			if (ret >= mid)ret = mid;
+			high = mid - 1;
+		}
+		else {
+			low = mid + 1;
+		}
+	}
+	return ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> n >> m;//n=아이들수 m=색상 수
+	int k, max = 0;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> k;//i번 색상 보석의 개수
+		if (max <= k)max = k;
+		v.push_back(k);
+	}
+	cout << bin(1, max);
+}
