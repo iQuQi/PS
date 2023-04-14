5dab04f913f2ec10aef0cb1083f9de745ef68162
iQuQi <kidscop99@naver.com>
2023-04-14 15:53:47 +0900

    graph - 이론 & 예제

---
+++ BOJ/08_그래프 이론/Kruskal/도시 분할 계획.py
@@ -0,0 +1,60 @@
+#
+
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+# N개의 집과 M개의 양방향 간선이 주어진다
+# M줄에 걸쳐 A B C가 주어지는데 A->B가 C비용이라는 것을 의미
+# 마을을 두개로 분리하려고 한다
+# 각 분리된 마을은 서로 연결되어야 하며, 유지비용을 최소로 하기 위해 필요없는 길은 없애려고 한다
+# 유지비 합의 최소값을 구하시오
+
+# =================
+# 2개의 최소 스패닝 트리 만들기
+# how? 하나의 최소 스패닝 트리를 만들고 가장 비용이 큰 간산 하나를 제거하면 된다
+
+# =================
+# 노드의 개수와 간선(Union 연산)의 개수 입력받기
+v, e = map(int, input().split())
+parent = [0] * (v + 1) # 부모 테이블 초기화
+
+# 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
+edges = []
+result = 0
+
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, v + 1):
+    parent[i] = i
+
+# 모든 간선에 대한 정보를 입력받기
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    # 비용순으로 정렬하기 위해서 튜플의 첫 번째 원소를 비용으로 설정
+    edges.append((cost, a, b))
+
+# 간선을 비용순으로 정렬
+edges.sort()
+last = 0 # 최소 신장 트리에 포함되는 간선 중에서 가장 비용이 큰 간선
+
+# 간선을 하나씩 확인하며
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+        last = cost
+
+print(result - last)
