086fa39c34379a10b54795cb7d6671d316a5c93f
iQuQi <kidscop99@naver.com>
2023-06-09 17:16:17 +0900

    이분탐색 실버 문제 추가 & DP 냅색추가

---
+++ 05_이분 탐색/2343_기타레슨.py
@@ -0,0 +1,57 @@
+'''
+강토는 자신의 기타 강의 동영상을 블루레이로 만들어 판매하려고 한다. 
+블루레이에는 총 N개의 강의가 들어가는데, 
+블루레이를 녹화할 때, 강의의 순서가 바뀌면 안 된다. 
+
+순서가 뒤바뀌는 경우에는 강의의 흐름이 끊겨, 학생들이 대혼란에 빠질 수 있기 때문이다. 즉, i번 강의와 j번 강의를 같은 블루레이에 녹화하려면 
+i와 j 사이의 모든 강의도 같은 블루레이에 녹화해야 한다.
+
+강토는 이 블루레이가 얼마나 팔릴지 아직 알 수 없기 때문에, 
+블루레이의 개수를 가급적 줄이려고 한다. 
+
+오랜 고민 끝에 강토는 M개의 블루레이에 모든 기타 강의 동영상을 녹화하기로 했다. 
+이때, 블루레이의 크기(녹화 가능한 길이)를 최소로 하려고 한다. 
+단, M개의 블루레이는 모두 같은 크기이어야 한다.
+
+강토의 각 강의의 길이가 분 단위(자연수)로 주어진다. 
+이때, 가능한 블루레이의 크기 중 최소를 구하는 프로그램을 작성하시오.
+'''
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+course = list(map(int,input().split()))
+total = sum(course)
+longest = max(course)
+min_length = total
+
+def possible(now):
+  tmp = 0
+  count = 1
+  
+  for c in course:
+    if tmp + c <= now:
+      tmp += c
+    else:
+      tmp = c
+      count += 1
+  return count <= m
+
+def solution(left,right):
+  global min_length
+  
+  while left <= right:
+    now = (left+right) // 2
+    if possible(now):
+      min_length = min(min_length, now)
+      right = now - 1
+    else:
+      left = now + 1
+
+  return
+
+
+# 오답이유 solution(1,total)로 함. 
+# 최소값은 모든 강의를 모두 다른 블루레이에 하나씩 넣는 것 -> 가장 긴 강의의 길이!
+solution(longest,total)
+print(min_length)
---
+++ 05_이분 탐색/6236_용돈관리.py
@@ -0,0 +1,57 @@
+'''
+현우는 용돈을 효율적으로 활용하기 위해 계획을 짜기로 하였다. 
+현우는 앞으로 N일 동안 자신이 사용할 금액을 계산하였고, 
+돈을 펑펑 쓰지 않기 위해 정확히 M번만 통장에서 돈을 빼서 쓰기로 하였다. 
+
+현우는 통장에서 K원을 인출하며, 통장에서 뺀 돈으로 하루를 보낼 수 있으면 
+그대로 사용하고, 모자라게 되면 남은 금액은 통장에 집어넣고 다시 K원을 인출한다. 
+
+다만 현우는 M이라는 숫자를 좋아하기 때문에, 
+정확히 M번을 맞추기 위해서 남은 금액이 그날 사용할 금액보다 많더라도 
+남은 금액은 통장에 집어넣고 다시 K원을 인출할 수 있다. 
+
+현우는 돈을 아끼기 위해 인출 금액 K를 최소화하기로 하였다. 
+현우가 필요한 최소 금액 K를 계산하는 프로그램을 작성하시오.
+'''
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+money = []
+for i in range(n):
+  money.append(int(input()))
+total = sum(money)
+biggest = max(money)
+min_k = total
+
+def possible(now):
+  have = now
+  out = 1
+  
+  for t in money:
+    if have - t < 0:
+      out += 1
+      have = now
+    have -= t
+  # 문제에서 정확히 m번 사용해야한다고 했으나
+  # 남은 돈이 넉넉함에도 다시 빼는게 가능하다고 함 
+  # 예를 들어, 5번꺼내야하는데 4번밖에 못꺼냈을 경우, 꺼낼 날 직전날에도 꺼냈다빼서 사용하게 되면 
+  # +1 가능이므로 out<=m으로 게산해도 무방
+  return out <= m
+
+def solution(left,right):
+  global min_k
+  
+  while left <= right:
+    now = (left+right) // 2
+    if possible(now):
+      min_k = min(min_k, now)
+      right = now - 1
+    else:
+      left = now + 1
+
+  return
+
+
+solution(biggest,total)
+print(min_k)
