716ff4f6afc79c9ef10690b5cc0dda5fff24a3fb
iQuQi <kidscop99@naver.com>
2023-08-16 03:11:45 +0900

    오답점검 - 5

--- 05_이분 탐색/1450_냅색문제.py
+++ 05_이분 탐색/1450_냅색문제.py
@@ -15,7 +15,7 @@ n, c = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # meet in the middle
-arr_1 = arr[:n//2]
+arr_1 = arr[:n//2]  # 반으로쪼개면 다룰 수 있는 범위로 줄어듬 (2^30 -> 2^15)
 arr_2 = arr[n//2:]
 
 subsum_a = []
@@ -26,10 +26,10 @@ for i in range(len(arr_1) + 1):
 
     for comb in comb_a:
         subsum_a.append(sum(comb))
-        
+
 for i in range(len(arr_2) + 1):
     comb_b = combinations(arr_2, i)
-    
+
     for comb in comb_b:
         subsum_b.append(sum(comb))
 
@@ -38,11 +38,11 @@ ans = 0
 
 # subsum_a와 subsum_b의 값을 더하는 작업을 이분탐색으로 진행
 for element_b in subsum_b:
-   
+
    # 이미 subsum_b의 값이 c를 넘었다면 작업 X
     if element_b > c:
-       continue
-    
+        continue
+
     start = 0
     end = len(subsum_a) - 1
 
@@ -51,10 +51,10 @@ for element_b in subsum_b:
 
         if subsum_a[mid] + element_b > c:
             end = mid - 1
-        
+
         else:
             start = mid + 1
-    
+
     ans += end + 1
 
 print(ans)
